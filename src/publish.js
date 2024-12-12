ea2276de5d724e1de91fafdf08ea0e0e27464db7
Tiago Almeida <tiagoterenas@gmail.com>
2024-12-12 16:14:12 +0000

    v1 registry fix

--- src/publish.js
+++ src/publish.js
@@ -2,7 +2,7 @@ import { exec } from "@actions/exec";
 
 async function main(options, ...args) {
     const scope = options.scope ? `@${options.scope}:registry` : "registry";
-    const registry = options.registry ? options.registry : "https://registry.npmjs.org";
+    const registry = options.registry ? options.registry : "https://registry.npmjs.org/";
     await exec("npm", ["config", "set", scope, registry]);
     if (token) await exec("npm", ["config", "set", "--", `${registry.replace(/^https?:/,"")}:_authToken`, token]);
     await exec("npm", ["publish"]);
