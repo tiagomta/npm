9062fe965e5fb99eaa8e3de57d2ef5326f4d7684
Tiago Almeida <tiagoterenas@gmail.com>
2024-12-12 16:19:24 +0000

    Change scope default private to public

--- src/publish.js
+++ src/publish.js
@@ -1,11 +1,15 @@
 import { exec } from "@actions/exec";
 
+const PUBLIC_REGISTRY = "https://registry.npmjs.org/";
+
 async function main(options, ...args) {
     const scope = options.scope ? `@${options.scope}:registry` : "registry";
-    const registry = options.registry ? options.registry : "https://registry.npmjs.org/";
+    const registry = options.registry ? options.registry : PUBLIC_REGISTRY;
     await exec("npm", ["config", "set", scope, registry]);
     if (token) await exec("npm", ["config", "set", "--", `${registry.replace(/^https?:/,"")}:_authToken`, token]);
-    await exec("npm", ["publish"]);
+    const publishArgs = ["publish"];
+    if (registry === PUBLIC_REGISTRY) publishArgs.push("--access", "public");
+    await exec("npm", publishArgs);
 }
 
 export default main;
