670aa65c5d0058c55b83f6673099c0f9466cb8a5
Tiago <tiagoterenas@gmail.com>
2025-07-04 10:11:02 +0100

    Always add latest tag

--- src/publish.js
+++ src/publish.js
@@ -1,16 +1,32 @@
 import { exec } from "@actions/exec";
+import fs from "node:fs";
 
 const PUBLIC_REGISTRY = "https://registry.npmjs.org/";
 
 async function main(options, ...args) {
-    const scope = options.scope ? `@${options.scope}:registry` : "registry";
-    const registry = options.registry ? options.registry : PUBLIC_REGISTRY;
-    await exec("npm", ["config", "set", scope, registry]);
-    if (token) await exec("npm", ["config", "set", "--", `${registry.replace(/^https?:/,"")}:_authToken`, token]);
-    const publishArgs = ["publish"];
-    if (registry === PUBLIC_REGISTRY) publishArgs.push("--access", "public");
-    if (options.tag) publishArgs.push("--tag", options.tag);
-    await exec("npm", publishArgs);
+  const scope = options.scope ? `@${options.scope}:registry` : "registry";
+  const registry = options.registry ? options.registry : PUBLIC_REGISTRY;
+  await exec("npm", ["config", "set", scope, registry]);
+  if (token)
+    await exec("npm", [
+      "config",
+      "set",
+      "--",
+      `${registry.replace(/^https?:/, "")}:_authToken`,
+      token,
+    ]);
+  const publishArgs = ["publish", "--tag", "latest"];
+  if (registry === PUBLIC_REGISTRY) publishArgs.push("--access", "public");
+  await exec("npm", publishArgs);
+  if (options.tag || options.tag !== "latest") {
+    const pkg = JSON.parse(await fs.promises.readFile("package.json", "utf-8"));
+    await exec("npm", [
+      "dist-tag",
+      "add",
+      `${pkg.name}@${pkg.version}`,
+      options.tag,
+    ]);
+  }
 }
 
 export default main;
