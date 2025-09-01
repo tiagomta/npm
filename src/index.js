1af6324ffe71b65d22345d658eebb2b729b787b4
Tiago Terenas Almeida <tiagoterenas@gmail.com>
2025-09-01 21:47:46 +0100

    #debug

--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@ import version from "./version.js";
 const commands = { install, publish, version };
 
 async function run() {
-    try {
-        const directory = core.getInput("working-directory");
-        if (directory) process.chdir(directory);
-        const [[command, ...args], options] = parse(core.getInput("command").trim());
-        globalThis.context = JSON.parse(core.getInput("context"));
-        globalThis.token = core.getInput("token");
-        if (!commands[command]) throw new Error(`Unknown command: ${command}`);
-        const result = commands[command](options, ...args);
-        if (result instanceof Promise) core.setOutput("result", await result);
-        else core.setOutput("result", result);
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+  try {
+    const directory = core.getInput("working-directory");
+    console.log(`Working directory: ${directory || process.cwd()}`);
+    if (directory) process.chdir(directory);
+    console.log(`Working directory: ${directory || process.cwd()}`);
+    const [[command, ...args], options] = parse(
+      core.getInput("command").trim()
+    );
+    globalThis.context = JSON.parse(core.getInput("context"));
+    globalThis.token = core.getInput("token");
+    if (!commands[command]) throw new Error(`Unknown command: ${command}`);
+    const result = commands[command](options, ...args);
+    if (result instanceof Promise) core.setOutput("result", await result);
+    else core.setOutput("result", result);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
